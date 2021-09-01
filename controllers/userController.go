68b8efc8d64caa3b3dacab96aa48effb789155cb
rjandonirahmana <rjandonirahmana@gmail.com>
2021-09-01 15:05:59 +0700

    feature struct model user and context

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,1 +1,22 @@
 package controllers
+
+import (
+	"github.com/labstack/echo/v4"
+
+	"book-api-demo/lib/database"
+	"net/http"
+)
+
+func GetUsers(c echo.Context) error {
+	users, err := database.GetUsers()
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get all users",
+		"users":   users,
+	})
+
+}
